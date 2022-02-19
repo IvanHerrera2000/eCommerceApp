bdb1e7acc14dd499a3a2ae1177f3b7c188e94c46
unknown <ivanalejandroherrera2000@gmail.com>
2022-02-19 12:01:39 -0300

    Setting up product model

---
+++ backend/models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = requiere("mongoose");
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    requiered: true,
+  },
+  description: {
+    type: String,
+    requiered: true,
+  },
+  price: {
+    type: Number,
+    requiered: true,
+  },
+  countInStock: {
+    type: Number,
+    requiered: true,
+  },
+  imageUrl: {
+    type: String,
+    requiered: true,
+  },
+});
+
+const Product = mongoose.model("product", productSchema);
+
+module.exports = Product;
