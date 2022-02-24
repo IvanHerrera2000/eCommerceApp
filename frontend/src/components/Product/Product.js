68344a32c2cbff6175fec741b9229f663b55a38c
unknown <ivanalejandroherrera2000@gmail.com>
2022-02-24 12:20:15 -0300

    Rendering the components in the home from the redux store

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -2,25 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Product.css";
 
-function Product() {
+function Product({ imageUrl, description, price, name, productId }) {
   return (
     <div className="product">
-      <img
-        src="https://images.unsplash.com/photo-1606813907291-d86efa9b94db?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80"
-        alt="name p"
-      />
+      <img src={imageUrl} alt={name} />
       <div className="product__info">
-        <p className="info__name">ps5</p>
-        <p className="info__description">
-          PlayStation 5 (PS5) is a home video game console developed by Sony
-          Interactive Entertainment. Announced in 2019 as the successor to the
-          PlayStation 4, the PS5 was released on November 12, 2020 in Australia,
-          Japan, New Zealand, North America, Singapore, and South Korea, and
-          November 19, 2020 onwards in other major markets except China and
-          India.
-        </p>
-        <p className="info__price">$499</p>
-        <Link to={`/product/${1111}`} className="info__button">
+        <p className="info__name">{name}</p>
+        <p className="info__description">{description.substring(0, 100)}...</p>
+        <p className="info__price">${price}</p>
+        <Link to={`/product/${productId}`} className="info__button">
           View
         </Link>
       </div>
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,15 +1,39 @@
 import React from "react";
 import Product from "../../components/Product/Product";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts as listProducts } from "../../redux/actions/productActions";
 import "./HomePage.css";
 
 function HomePage() {
+  const dispatch = useDispatch();
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products, loading, error } = getProducts;
+
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
+
   return (
     <div className="homescreen">
-      <h2 className="homescreen__title">Lastest Products</h2>
+      <h2 className="homescreen__title">Latest Products</h2>
       <div className="homescreen__products">
-        <Product />
-        <Product />
-        <Product />
+        {loading ? (
+          <h2>Loading...</h2>
+        ) : error ? (
+          <h2>{error}</h2>
+        ) : (
+          products.map((product) => (
+            <Product
+              key={product._id}
+              name={product.name}
+              description={product.description}
+              price={product.price}
+              imageUrl={product.imageUrl}
+              productId={product._id}
+            />
+          ))
+        )}
       </div>
     </div>
   );
